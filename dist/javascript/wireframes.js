29d957c22f0067c2dea647e29b0d3ce46903f676
Charles Razack <charles@ldd.fr>
2017-04-21 19:17:41 +0200

    Add code to hide, for the moment the toolbar is 'en dur' in the HTML, it will have to be added dynamically

--- dist/javascript/wireframes.js
+++ dist/javascript/wireframes.js
@@ -16,17 +16,27 @@ function css_wireframes_init(){
 			// Add the close button to each optional element
 			var elements = document.getElementsByClassName('wf-optional');
 			for (var i = 0; i < elements.length; i++) {
-				elements[i].insertAdjacentHTML('beforeEnd', '<a class="wf-optional__close">&times;</a>');
+				elements[i].insertAdjacentHTML('beforeEnd', '<a class="wf-optional__close" data-btn="hide-element">&times;</a>');
 			}
 
 			// Hide optional elements on click
 			var btns = document.getElementsByClassName('wf-optional__close');
-			for (var i = 0; i < elements.length; i++) {
+			for (var i = 0; i < btns.length; i++) {
 				btns[i].addEventListener("click", function(e){
 					e.preventDefault();
-					this.parentElement.className += ' wf-hidden';
+					this.parentElement.classList.add('wf-hidden');
 				});
 			}
+
+			// Show all hidden elements on click
+			var btn_show = document.getElementById('show-elements');
+			var hidden = document.getElementsByClassName('wf-hidden');
+			btn_show.addEventListener("click", function(e){
+				e.preventDefault();
+				for (var i = 0; i < hidden.length; i++) {
+					hidden[i].classList.remove('wf-hidden');
+				}
+			});
 
 		}
 		manage_optional_elements();
