269f9202b3bc67818f23c651583466d095e6f7bd
Charles Razack <charles@ldd.fr>
2017-04-21 16:12:23 +0200

    Move the dist CSS and JS in subdirectories, add JS minification, plus the **very** begining of the optionnal JS

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var minify = require('gulp-minify');
 var rename = require("gulp-rename");
 var cssBase64 = require('gulp-css-base64');
 
 // Default task
-gulp.task('default', function() {
-});
+gulp.task('default', function() {});
 
 // Task : monitor changes on SASS files, launch the compilation and post-compilation stuff
 gulp.task('watch', function() {
-    return gulp.watch('./src/**', gulp.series('sass', 'autoprefixer', 'base64', 'minify'));
+    return gulp.watch('./src/**', gulp.series('sass', 'autoprefixer', 'base64', 'minify-css', 'minify-js'));
 });
 
 // Task : compile the main SASS file to CSS
@@ -23,35 +23,48 @@ gulp.task('sass', function() {
         //.pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         //.pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist/css'));
 });
 
 // Task : add vendor prefixes to the compiled CSS file
 gulp.task('autoprefixer', function() {
-    return gulp.src('./dist/*.css')
+    return gulp.src('./dist/css/*.css')
         .pipe(postcss(
             [autoprefixer()]
         ))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist/css'));
 });
 
 // Task : converts all data found within a stylesheet into base64-encoded data URI strings
-gulp.task('base64', function () {
-    return gulp.src('./dist/*.css')
+gulp.task('base64', function() {
+    return gulp.src('./dist/css/*.css')
         .pipe(cssBase64({
             //baseDir: "./src/images" // doesn't seem to work ?
         }))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist/css'));
 });
 
 // Task : minify the compiled CSS file in a new .min.css file
-gulp.task('minify', function() {
-    return gulp.src('./dist/wireframes.css')
+gulp.task('minify-css', function() {
+    return gulp.src('./dist/css/wireframes.css')
         .pipe(cleanCSS({
             compatibility: 'ie8'
         }))
-        .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest('./dist'));
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+// Task : minify the JS file in a new .min.js file
+gulp.task('minify-js', function() {
+    gulp.src('./src/javascript/*.js')
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            }
+        }))
+        .pipe(gulp.dest('./dist/javascript'))
 });
 
 // Go go go !
