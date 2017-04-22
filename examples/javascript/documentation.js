2b6302ccf678c102a38f8c65bf2650cf80ade442
Charles Razack <charles@ldd.fr>
2017-04-22 03:14:53 +0200

    Add tooltips on the documentation

---
+++ examples/javascript/documentation.js
@@ -0,0 +1,9 @@
+/**
+ * CSS wireframe documentation
+ */
+jQuery(function ($) {
+
+  // Fire tooltips on all elements with a title in the overview
+  $('#overview [title]').tipsy({opacity: 0.8});
+
+});
